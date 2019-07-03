2accafbd16cc56b9ae8e74da95210bf206542102
Saurav Mohan V <sauravmohan2@gmail.com>
2019-07-03 17:43:28 +0530

    Cors headers changed
    Cors headers changed to node middle wear cors.

--- APIs/near-by-search.js
+++ APIs/near-by-search.js
@@ -8,14 +8,14 @@ APIKEY = "AIzaSyC7qvCoesb0g_CD0lfCBR1rq-ksUzQY7m4";
 // type = "resturant";
 // keyword = "Cruise";
 // --------------------------------------------------- 
-//----Test using this URL in specified port: localhost:_portnumber_/searcharea/searchkey?lat=-33.8670522&lng=151.1957362&radius=1500&type=resturant&keyword=Cruise //
+//----Test using this URL in specified port: localhost:_portnumber_/nearbysearch/lat=-33.8670522&lng=151.1957362&radius=1500&type=resturant&keyword=Cruise //
 module.exports = function (app, db) {
     app.get('/nearbysearch/lat=:lat&lng=:lng&radius=:radius&type=:type&keyword:keyword', (req, res) => {
         axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + req.params.lat + ',' +  req.params.lng + '&radius=' +  req.params.radius + '&type=' +  req.params.type + '&keyword='+  req.params.keyword + '&key=' + APIKEY + '')
         .then(function (response) {
 
             if (response.data.status == "OK") {
-                res.send(response.data.results);
+                res.send(response.data);
             }
             else {
                 res.status(400).send("Error Status - 400 - Bad Request Error");
--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
-const port = 4200;
+const port = 8000;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 require('./APIs')(app, {});
 app.listen(port, () => {
